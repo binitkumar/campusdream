9aeff90a84cc2151ec22755591fbea091f9a8197
Binit Kumar <binit.kumar.hbs@gmail.com>
2013-09-14 03:50:28 +0530

    Books category

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ActiveRecord::Base
-  attr_accessible :description, :name, :content
+  attr_accessible :description, :name, :content, :category
 
   has_attached_file :content, {}.merge(PAPERCLIP_STORAGE_OPTIONS)
 end
